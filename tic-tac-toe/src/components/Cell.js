bdba4fccb95be022b42e34090b35e55076c6837f
General <general-m7@yandex.ru>
2021-03-02 21:26:34 +0300

    feat: delete files

--- tic-tac-toe/src/components/Cell.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Cell(props) {
-    return (
-
-        <div className="cell" onClick={props.onClick} onChange={props.onChange}>{props.value} </div>
-
-    )
-}
