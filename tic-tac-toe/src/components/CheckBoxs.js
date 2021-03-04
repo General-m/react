2ead8a0e59bbf98cadb56f7b949bbe5999df584b
General <general-m7@yandex.ru>
2021-03-05 01:14:59 +0300

    feat: new test build

--- tic-tac-toe/src/components/CheckBoxs.js
+++ tic-tac-toe/src/components/CheckBoxs.js
@@ -3,22 +3,19 @@ import Radio from '@material-ui/core/Radio';
 
 
 
-export default function RadioButtons() {
+export default function CheckBoxs() {
     const [icons, setIconsView] = useState('0');
     const settingsState = {
-        icons: icons
+        iconsv: icons
     }
-    useEffect(() => {
-        localStorage.setItem('gameGeneralSettings', JSON.stringify(settingsState));
-    }, [icons])
+    // useEffect(() => {
+    //     localStorage.setItem('gameGeneralSettings', JSON.stringify(settingsState));
+    // }, [icons])
 
     const handleChange = (event) => {
         setIconsView(event.target.value);
-        console.log(event.target.value)
+        console.log(icons)
     };
-
-
-
 
     return (
         <div className='view__settings'>
@@ -27,22 +24,24 @@ export default function RadioButtons() {
                 onChange={handleChange}
                 value='0'
                 color="default"
-                name="radio-button-demo"
-                inputProps={{ 'aria-label': 'E' }}
+                // name="radio-button-demo"
+                // inputProps={{ 'aria-label': 'E' }}
                 size="small"
             />
             <div className='icons'>
                 ╳◯
             </div>
+
             <Radio
                 checked={icons === '1'}
                 onChange={handleChange}
                 value='1'
                 color="default"
-                name="radio-button-demo"
-                inputProps={{ 'aria-label': 'E' }}
+                // name="radio-button-demo"
+                // inputProps={{ 'aria-label': 'E' }}
                 size="small"
             />
+
             <div className='icons'>
                 🔥❄
             </div>
